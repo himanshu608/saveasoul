f29c4ba99828e8ce41ae29a252337228b1cd94af
himansuuverma <himansuuverma@gmail.com>
2025-09-15 18:07:26 +0000

    initial project setup

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text> Hello </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
